ea000b4e46d6a4278535c58d42c6ca8af3223f76
Wojciech Czerniak <wojciech.czerniak@gmail.com>
2018-05-17 23:25:02 +0200

    Add event listener for DOMContentLoaded.

--- vue/example-1/demo.js
+++ vue/example-1/demo.js
@@ -1,16 +1,18 @@
-new Vue({
-  el: "#vue-app",
-  data: function() {
-    return {
-      settings: {
-        data: Handsontable.helper.createSpreadsheetData(50, 50),
-        colHeaders: true,
-        width: 500,
-        height: 500
-      }
-    };
-  },
-  components: {
-    HotTable
-  }
-});
+document.addEventListener('DOMContentLoaded', function(){
+  new Vue({
+    el: "#vue-app",
+    data: function() {
+      return {
+        settings: {
+          data: Handsontable.helper.createSpreadsheetData(50, 50),
+          colHeaders: true,
+          width: 500,
+          height: 500
+        }
+      };
+    },
+    components: {
+      HotTable
+    }
+  });
+}, false);
